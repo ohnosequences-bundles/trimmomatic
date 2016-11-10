c3f89d309e11bb2ffedcf72a1ba20099760b5a32
Alexey Alekhin <laughedelic@gmail.com>
2016-11-10 18:16:09 +0100

    Wrote basic code for installing and running Trimmomatic

--- src/main/scala/Trimmomatic.scala
+++ src/main/scala/Trimmomatic.scala
@@ -1,5 +1,21 @@
-package ohnosequences-bundles.trimmomatic
+package ohnosequencesBundles.statika
 
-class Trimmomatic {
+import ohnosequences.statika._
+import java.io.File
 
+abstract class Trimmomatic(val version: String) extends Bundle() { trimmomatic =>
+
+  lazy val baseName = s"Trimmomatic-${version}"
+  lazy val trimmomaticZip = s"${baseName}.zip"
+  lazy val jar = new File(trimmomatic.baseName, s"trimmomatic-${version}.jar")
+
+  lazy val downloadZip = cmd("wget")(
+    s"http://s3-eu-west-1.amazonaws.com/resources.ohnosequences.com/trimmomatic/${version}/${trimmomaticZip}"
+  )
+
+  def instructions: AnyInstructions =
+    downloadZip -&-
+    cmd("unzip")(trimmomaticZip)
+
+  def run(args: String*): CmdInstructions = cmd("java")("-jar", trimmomatic.jar.getCanonicalPath, args: _*)
 }
